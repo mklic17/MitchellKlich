525b3e3e01c9937915c7251cf0846530f17cd74a
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-07-06 01:15:04 -0400

    added some projects intoand short descriptions to the HTML and setup event handlers for them in the javascript.

--- js/app.js
+++ js/app.js
@@ -2,17 +2,37 @@ $(document).foundation()
 
 var Mitch = {
 
-init: function() {
-  this.setupEventHandlers();
-},
+  init: function() {
+    this.setupEventHandlers();
+  },
 
-setupEventHandlers: function() {
-  $('#enter').on('submit', this.enterTransition);
-},
+  setupEventHandlers: function() {
+    // $('#enter').on('submit', this.enterTransition);
+    $('.mutantProject').on('click', this.mutantProject);
+    $('.responderProject').on('click', this.responderProject);
+    $('.pokemonProject').on('click', this.pokemonProject);
+    $('.cardProject').on('click', this.cardProject);
+  },
 
-enterTransition: function(ev) {
-  console.log("here");
-},
+  mutantProject: function(ev) {
+    console.log('made it to mutantProjects()');
+  },
+
+  responderProject: function(ev) {
+    console.log('made it to responderProjects()');
+  },
+
+  pokemonProject: function(ev) {
+    console.log('made it to pokemonProjects()');
+  },
+
+  cardProject: function(ev) {
+    console.log('made it to cardProjects()');
+  },
+
+  enterTransition: function(ev) {
+    console.log("here");
+  },
 
 
 }
