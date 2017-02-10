aea0527141b05989def002e6fa7aad5b37376f5c
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2017-02-10 14:00:18 -0500

    added a transition to the homepage

--- js/app.js
+++ js/app.js
@@ -24,64 +24,72 @@ $(window).scroll(function () {
     }
 });
 
-
-var Mitch = {
-
-  init: function() {
-    this.setupEventHandlers();
-  },
-
-  setupEventHandlers: function() {
-    // $('#enter').on('submit', this.enterTransition);
-    $('.mutantProject').on('click', this.mutantProject);
-    $('.responderProject').on('click', this.responderProject);
-    $('.pokemonProject').on('click', this.pokemonProject);
-    $('.cardProject').on('click', this.cardProject);
-    $('.contactMe').on('click', this.contactMe);
-  },
-
-  createProjectObject() {
-    return projectSummary = {
-      title: '',
-      pictureLocation: '',
-      description: '',
-      futureExpansions: '',
-    }
-  },
-  contactMe: function(ev) {
-    ev.preventDefaults;
-    window.location.href = 'mailto:MitchellKlich_2017@DePauw.edu?subject=ContactMe&=Hello';
-  },
-
-  mutantProject: function(ev) {
-
-    console.log('made it to mutantProjects()');
-    projectSummary = Mitch.createProjectObject();
-    projectSummary.title = 'Mutant Project';
-    // projectSummary.pictureLocations = '';
-    projectSummary.description = "Mutant Office Hour Project";
-    projectSummary.futureExpansions = "Future things to do ";
-    console.log(projectSummary);
-
-  },
-
-  responderProject: function(ev) {
-    console.log('made it to responderProjects()');
-  },
-
-  pokemonProject: function(ev) {
-    console.log('made it to pokemonProjects()');
-  },
-
-  cardProject: function(ev) {
-    console.log('made it to cardProjects()');
-  },
-
-  enterTransition: function(ev) {
-    console.log("here");
-  },
+$(window).ready(function() {
+    var x = $("#contactLinks");
+    setTimeout(function() {
+        x.addClass("animated fadeInUpBig");
+    }, 350);
 
 
-}
+});
 
-Mitch.init();
+
+// var Mitch = {
+//   init: function() {
+//     this.setupEventHandlers();
+//   },
+//
+//   setupEventHandlers: function() {
+//     // $('#enter').on('submit', this.enterTransition);
+//     $('.mutantProject').on('click', this.mutantProject);
+//     $('.responderProject').on('click', this.responderProject);
+//     $('.pokemonProject').on('click', this.pokemonProject);
+//     $('.cardProject').on('click', this.cardProject);
+//     $('.contactMe').on('click', this.contactMe);
+//   },
+//
+//   createProjectObject() {
+//     return projectSummary = {
+//       title: '',
+//       pictureLocation: '',
+//       description: '',
+//       futureExpansions: '',
+//     }
+//   },
+//   contactMe: function(ev) {
+//     ev.preventDefaults;
+//     window.location.href = 'mailto:MitchellKlich_2017@DePauw.edu?subject=ContactMe&=Hello';
+//   },
+//
+//   mutantProject: function(ev) {
+//
+//     console.log('made it to mutantProjects()');
+//     projectSummary = Mitch.createProjectObject();
+//     projectSummary.title = 'Mutant Project';
+//     // projectSummary.pictureLocations = '';
+//     projectSummary.description = "Mutant Office Hour Project";
+//     projectSummary.futureExpansions = "Future things to do ";
+//     console.log(projectSummary);
+//
+//   },
+//
+//   responderProject: function(ev) {
+//     console.log('made it to responderProjects()');
+//   },
+//
+//   pokemonProject: function(ev) {
+//     console.log('made it to pokemonProjects()');
+//   },
+//
+//   cardProject: function(ev) {
+//     console.log('made it to cardProjects()');
+//   },
+//
+//   enterTransition: function(ev) {
+//     console.log("here");
+//   },
+//
+//
+// }
+//
+// Mitch.init();
